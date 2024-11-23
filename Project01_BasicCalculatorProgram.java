ec6c0fa5a7a17d126e8d0dae646afbb4198ac557
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2024-11-23 17:58:39 +0700

    Rename file from BasicCalculatorProgram.java to Project01_BasicCalculatorProgram.java

--- Project01_BasicCalculatorProgram.java
+++ Project01_BasicCalculatorProgram.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class BasicCalculatorProgram {
+class Project01_BasicCalculatorProgram {
     public static void main (String[] args) {   
         Scanner sc = new Scanner(System.in);
         System.out.println("== Basic calculator ===");
