dd76f8bc24bea85c3425dd705b443d7c6e359de8
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2024-11-27 20:26:42 +0700

    Add Project05_ProgramToFindFactorsOfNumbers.java

---
+++ Project05_ProgramToFindFactorsOfNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Project05_ProgramToFindFactorsOfNumbers {
+    public static void main (String[]args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int number = sc.nextInt();
+
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    if (number == i) {
+                        System.out.print(i);
+                    }else {
+                        System.out.print(i + ", ");
+                    }
+                }
+        }
+    }
+}
+}
