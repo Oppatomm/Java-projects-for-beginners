1f1a2c91d4593fd722377cccca23e8d123788462
Sumate Boonkird <sumateboonkird5555github@gmail.com>
2024-12-07 02:50:27 +0700

    Add introductory message to Project04_ProgramToCheckLeapYear.java - Display "=== Program To Check Leap Year ===" at the start - Improves user experience and clarity

--- Project04_ProgramToCheckLeapYear.java
+++ Project04_ProgramToCheckLeapYear.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 public class Project04_ProgramToCheckLeapYear {
     public static void main (String[]args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("=== Program To Check Leap Year ===");
         System.out.printf("Enter year (AD): ");
         int year = sc.nextInt();
-        if (year % 4 != 0) {
-            System.out.println("Year");
-        }else if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println("2Year");
-        }else if (year % 100 == 0 && year % 400 != 0) {
-            System.out.println("3Year");
-        }else if (year % 400 == 0 && year % 100 == 0) {
-            System.out.println(year + " is a Leap year.");
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is a Leap year."); 
+                } else {
+                    System.out.println(year + " is not a Leap year."); 
+                }
+            } else {
+                System.out.println(year + " is a Leap year.");
+            }
+        } else {
+            System.out.println(year + " is not a Leap year.");
         }
         sc.close();
     }
